83805d77a74bdbdf8998752c0e883bdc5a96c229
joeragheb29 <youssifragheb838@gmail.com>
2026-02-10 19:24:27 +0200

    Add Toaster

--- MSP_Attendance_Dashboard/Frontend/src/App.tsx
+++ MSP_Attendance_Dashboard/Frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Hero from './pages/Hero'
+import Login from './pages/Login'
 import AdminInterface from './pages/Admin_Interface'
 import ProtectedRoute from './components/ProtectedRoute'
 import Register from './pages/Register'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path="/" element={<Hero />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={
           <ProtectedRoute>
